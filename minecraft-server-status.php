69a64516375d48fcd80074747b847c8d106e000e
dparker1005 <dlparker1005@gmail.com>
2017-06-08 18:01:51 -0400

    Add files via upload

---
+++ minecraft-server-status.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package minecraft-server-status
+ * @version 0.1
+ */
+/*
+Plugin Name: Minecraft Server Status
+Description: Shows information about a minecraft server
+Author: David Parker
+Version: 0.1
+*/
+function mc_server_status($atts){
+
+	$server = $atts['server'];
+	$to_return = "";
+	$to_return = $to_return."<link rel='stylesheet' type='text/css' href='../wp-content/plugins/minecraft-server-status/minecraft-server-status.css' />";
+	$raw_server_data = file_get_contents('https://mcapi.ca/query/'.$server.'/info');
+	//echo $raw_server_data;
+	$server_data = json_decode($raw_server_data, true);
+	$to_return = $to_return."<div class='mc_server_div'>";
+	$to_return = $to_return."<h3 class='mc_hostname'>".$server_data['hostname']."</h3>";
+	if($server_data['status'] == 1){
+		$to_return = $to_return."<p class='mc_green'>Online</p>"; 
+		$players_online = $server_data['players']['online'];
+		$players_max = $server_data['players']['max'];
+		if($players_online >= $players_max){
+			$to_return = $to_return."<p class='mc_red'> ";
+		}
+		elseif($players_online >= $players_max*.9){
+			$to_return = $to_return."<p class='mc_yellow'> ";
+		}
+		else{
+			$to_return = $to_return."<p class='mc_green'> ";
+		}
+		
+		$to_return = $to_return.$players_online."/".$players_max." players online</p>";
+		$to_return = $to_return."<p>Ping: ".$server_data['ping']."</p>";
+		$to_return = $to_return."<p>Minecraft version ".$server_data['version']."</p>";
+	}
+	else{
+		$to_return = $to_return."<p class='mc_red'>Offline</p>"; 
+	}
+	$to_return = $to_return."</div>";
+	return $to_return;
+}
+add_shortcode('mc_server_status', 'mc_server_status');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,16 @@
+=== Minecraft Server Status ===
+Contributors: David Parker
+Version: 0.1
+
+——— Description ———
+This plugin will display information about a Mincraft server
+using an API call to https://mcapi.ca/query/play.minesuperior.com/info.
+
+Information includes whether the server is online, the number of players
+online, the maximum allowed number of players on the server, the ping,
+and the version of Minecraft that the server is running.
+
+
+——— INSTRUCTIONS ———
+Information can be displayed on a Wordpress website by using the
+shortcode [mc_server_status server=“INSERT_SERVER_HERE”]. 
